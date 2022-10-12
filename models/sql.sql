f842d0ea7ae1b27597cab2d204ace72e34e60f63
vominhthongss <vominhthongss@gmail.com>
2022-10-12 12:30:38 +0700

    update sql

--- models/sql.sql
+++ models/sql.sql
@@ -10,3 +10,17 @@ WHERE post.USER_NAME=user.USER_NAME)
 SELECT * FROM (SELECT * FROM evaluation) as A,
 (SELECT post.POST_ID,post.POST_CONTENT,user.USER_NAME, user.USER_AVATARURL FROM post, user
 WHERE post.USER_NAME=user.USER_NAME) as B
+
+SELECT * FROM (SELECT post.POST_ID,post.POST_CONTENT,user.USER_NAME, user.USER_AVATARURL FROM post, user
+WHERE post.USER_NAME=user.USER_NAME) as A
+LEFT JOIN
+(SELECT * FROM evaluation) as B
+ON A.POST_ID=B.POST_ID
+
+SELECT * FROM (SELECT post.POST_ID,post.POST_CONTENT,user.USER_NAME, user.USER_AVATARURL FROM post, user
+WHERE post.USER_NAME=user.USER_NAME) as A
+LEFT JOIN
+(SELECT post.POST_ID,post.POST_CONTENT,film.FILM_ID, film.FILM_NAME, evaluation.EVALUATION_RATE, evaluation.USER_NAME, user.USER_AVATARURL FROM post, user, evaluation, film
+WHERE post.USER_NAME=user.USER_NAME and post.POST_ID=evaluation.POST_ID and user.USER_NAME=evaluation.USER_NAME and evaluation.FILM_ID=film.FILM_ID
+AND post.POST_ISDELETE=0) as B
+ON A.POST_ID=B.POST_ID
